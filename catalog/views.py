9a0e67bfb8339e7cf781060def65d4f29ba0310e
julia <juliakofmana@gmail.com>
2023-12-25 00:02:22 +0300

    Homework_20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
